539e56f045693e954596ba2512cea36623fb207d
123kishan <mishrakishan085@gmail.com>
2023-08-08 17:59:05 +0530

    adding delete button

--- expensetrackerreact/src/Components/Expense.js
+++ expensetrackerreact/src/Components/Expense.js
@@ -3,11 +3,15 @@ import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 function Expense(props)
 {
+    const clickHandler=(e)=>{
+        e.target.parentNode.remove();
+    }
     return (
         <>
         <div className="expense-item">
         <ExpenseDate date={props.date}/>
         <ExpenseDetails title={props.title} amount={props.amount} location={props.location}/>
+        <button onClick={clickHandler}>Delete Expense</button>
         </div>
         </>
 
